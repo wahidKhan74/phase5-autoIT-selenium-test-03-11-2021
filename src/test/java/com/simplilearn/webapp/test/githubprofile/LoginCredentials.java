cb321388bd03143c973b947e85b4615da0fb482c
waheed khan <wahid---@gmail.com>
2021-03-11 22:34:25 +0530

    update

--- src/test/java/com/simplilearn/webapp/test/githubprofile/LoginCredentials.java
+++ src/test/java/com/simplilearn/webapp/test/githubprofile/LoginCredentials.java
@@ -2,6 +2,6 @@ package com.simplilearn.webapp.test.githubprofile;
 
 public class LoginCredentials {
 
-	public static String username="";
-	public static String password="";
+	public static String username="abcd@gmail.com";
+	public static String password="abcd@123";
 }
